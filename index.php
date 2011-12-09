282cb6613d05e1c6ba57d986fb64ec045b1ffb17
James Hoyland <jameshoyland@yahoo.co.uk>
2011-12-09 13:55:13 +0100

    Signed-off-by: James Hoyland <jameshoyland@yahoo.co.uk>

--- index.php
+++ index.php
@@ -3,10 +3,29 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
+        <script type="text/javascript">
+            function startUp() {
+            if (window.XMLHttpRequest)
+              {
+                xhttp=new XMLHttpRequest();
+              }
+            else // IE 5/6
+              {
+                xhttp=new ActiveXObject("Microsoft.XMLHTTP");
+              }
+            xhttp.open("GET","strinXtest.xml",false);
+            xhttp.send();
+            xmlDoc=xhttp.responseXML; 
+            
+            var grt = xmlDoc.getElementById("greeting");
+            
+            var outp = grt.getElementByTagNameNS();
+            
+            document.getElementById("output").innerHTML = "fuck";
+            }
+        </script>
     </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
+    <body onload="startUp()">
+        <span id="output">Hi</span>
     </body>
 </html>
